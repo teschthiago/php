4d9f1c3eddb73cfc9b8f793bb67b275bee7d4df3
teschthiago <63598734+teschthiago@users.noreply.github.com>
2020-04-26 20:07:33 -0300

    Added some css and bars

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-﻿<?php
-echo "ハロー　ザ　ワールド";
-?>
+<?php require "header.php"; ?>
+      <div class="container-fluid">
+        <h1 class="mt-4">
+            <Center>
+            <br/><br/>
+                  <?php
+                  /*
+                  ドドド
+                  ドドド
+                  */
+                  echo "ハロー　ザ　ワールド";
+                  //時よ止まれ
+                  ?>
+            </Center></h1>
+        <p>text 1</p>
+        <p><code>text 2</code></p>
+      </div>
+  <?php require "footer.php"; ?>
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<!-- Sidebar -->
+<div class="bg-light border-right" id="sidebar-wrapper">
+  <div class="sidebar-heading">SYSTEM TABLES </div>
+  <div class="list-group list-group-flush">
+    <a href="#" class="list-group-item list-group-item-action bg-light">Persons</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Employees</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Addresses</a>
+  </div>
+</div>
+<!-- /#sidebar-wrapper -->
+
+
